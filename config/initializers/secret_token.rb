2715892833e73f193c1d36d9640f98d343f87b47
Stephen Wright <wright.net@gmail.com>
2010-11-28 10:53:57 -0600

    initial load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uccnrails::Application.config.secret_token = '4fabaa792892d2009aa71d28bb3526c36cf5c672d15e9542a1301d9a04f4af5c9c6ad168369f9a11ed55aa19130bb82ab2f0b1ac433a450dc0067224850cc842'
