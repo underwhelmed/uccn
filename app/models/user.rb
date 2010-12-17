5ae6e93b57efcca108aff4a5f9423b9ca7f6c96e
Stephen Wright <wright.net@gmail.com>
2010-12-16 23:09:21 -0600

    added more fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :login, :first_name, :last_name
   
-  attr_accessible :login, :password, :password_confirmation, :remember_me
+  attr_accessible :login, :first_name, :last_name, :business_name, 
+    :address1, :address2, :city, :state, :zip_code,
+    :password, :password_confirmation, :remember_me
   
 end
