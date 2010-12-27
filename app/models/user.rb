8bb3100e822917139f804334c1453326024e96eb
Stephen Wright <wright.net@gmail.com>
2010-12-27 15:04:31 -0600

    fixed layout of forms, prettied up the index for users to display the correct last login time

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :login
   validates_presence_of :login, :first_name, :last_name
   
-  attr_accessible :login, :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :admin
-  
+  attr_accessible :login, :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :active, :admin
+    
+  def name
+    if self.display_name.nil?
+      self.first_name + ' ' + self.last_name
+    else
+      self.display_name
+    end
+  end
 end
