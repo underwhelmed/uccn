5e93c404609c7a4ce08c823132c40b657286b39e
Stephen Wright <wright.net@gmail.com>
2010-12-26 22:54:46 -0600

    got edit and update working for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -20,4 +20,37 @@ class Admin::UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        redirect_to(edit_admin_user_path(@user), :notice => 'User was successfully updated.') 
+        format.xml  { head :ok }
+      else
+        format.html { render :action => :edit }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    begin
+      @user.destroy
+      flash[:notice] = "User #{@user.name} deleted"
+    rescue Exception => e
+      flash[:notice] = e.message
+    end
+    
+    respond_to do |format|
+      format.html { redirect_to(admin_users_url) }
+      format.xml  { head :ok }
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-
+    
+  validates_uniqueness_of :login
   validates_presence_of :login, :first_name, :last_name
   
   attr_accessible :login, :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
