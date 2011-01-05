72e1deb139a2fe65ca33d5a736f7168bf07e632a
Stephen Wright <wright.net@gmail.com>
2011-01-04 21:49:48 -0600

    removed old welcome email mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "no-reply@uccn.org"
-  
-  def welcome_email(user)
-    @user = user
-    @url = 'http://www.uccn.org'
-    mail(:to =>user.email, :subject => "Welcome to the UCCN")
-  end
-end
