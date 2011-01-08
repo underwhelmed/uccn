67e95c3345d6553e31f6a232908769b4c4e101a7
Stephen Wright <wright.net@gmail.com>
2011-01-08 11:32:54 -0600

    fixed tests not passing

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -1,18 +1,20 @@
 require "spec_helper"
 
 describe Notifier do
+ 
   describe "user_feedback" do
-    let(:mail) { Notifier.user_feedback }
+    let(:mail) { Notifier.user_feedback("Hi") }
 
     it "renders the headers" do
-      mail.subject.should eq("User feedback")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("Feedback from the UCCN website")
+      mail.to.should eq(["irishpeg@comcast.net"])
+      #mail.from.should eq(["from@example.com"])
     end
 
     it "renders the body" do
       mail.body.encoded.should match("Hi")
     end
+    
   end
 
 end
