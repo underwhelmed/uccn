fa650f2a1d9c467c23036268dd840da649b581f8
Stephen Wright <wright.net@gmail.com>
2011-01-13 20:30:24 -0600

    added validation for adding new event

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -9,7 +9,13 @@ class Admin::EventsController < AdminController
   end
   
   def create
-    
+     @event = Event.new(params[:event])
+
+      if @event.save
+        redirect_to(admin_events_url, :notice => "Event was successfully created.")
+      else
+        render :action => :new
+      end
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
   has_event_calendar
   
-  attr_accessible :name
+  validates_presence_of :name, :description, :start_at, :members_only, :all_day
+  
+  attr_accessible :name, :description, :start_at, :members_only, :all_day, :start_at, :end_at
 end
