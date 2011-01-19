bab8893c313ade9b6a8141363c6fcb68f1380ef7
Stephen Wright <wright.net@gmail.com>
2011-01-18 19:42:25 -0600

    added day view for calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -9,6 +9,11 @@ class CalendarController < ApplicationController
     @event_strips = Event.event_strips_for_month_for_members(@shown_month, user_signed_in?)
   end
   
+  def day
+    @full_date = "January 11, 2011"
+    @events = Event.all
+  end
+  
   def event
     @event = Event.find(params[:id])
     redirect_to calendar_path, :notice => 'You are not signed in and cannot view this event' if (!user_signed_in? && @event.members_only)  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Uccnrails::Application.routes.draw do
   match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
+  match "/calendar/:year/:month/:day", :controller => "calendar", :action => "day"
 
   match 'events/:id' => 'calendar#event'
 
