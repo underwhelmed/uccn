c4881cbdfdccd315f942ae1786e53f501d8de14d
Stephen Wright <wright.net@gmail.com>
2011-01-30 22:51:50 -0600

    fixed some functionality when adding and deleting blog categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,33 @@
 class Admin::CategoriesController < AdminController
 
-def index
-  @categories = Category.all
-  @category = Category.new
-end
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(params[:category])   
+    if @category.save
+      redirect_to admin_categories_url, :notice => "Category was successfully created."
+    else
+      @categories = Category.all
+      render :action => 'index'
+    end
+  end
+  
+  def destroy
+     @categories = Category.find(params[:id])
+      begin
+        @categories.destroy
+        flash[:notice] = "Category deleted"
+      rescue Exception => e
+        flash[:notice] = e.message
+      end
+
+      respond_to do |format|
+        format.html { redirect_to(admin_categories_url) }
+        format.xml  { head :ok }
+      end
+  end
 
 end
