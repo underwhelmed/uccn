5e7e3d4b8c95b93317d48b9d0e889b609df0bd96
Stephen Wright <wright.net@gmail.com>
2011-02-03 23:07:30 -0600

    updated blog and post detail for user

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,14 @@
 class BlogController < ApplicationController
   
   def index
+    #need to update this so it will only return posts and categories from appropriate posts (members_only)
     @posts = Post.paginate :per_page => 5, :page => params[:page], :order => 'published_at DESC'
+    @categories = Category.find(:all, :order => "name")
   end
   
   def show
-    @post = Post.find_by_slug(params[:slug])
+    @post = Post.find_by_slug(params[:slug])    
+    @categories = Category.find(:all, :order => "name")
     if !@post
       render "404"
     end
