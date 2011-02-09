0eb002b31b8ca48c897280d9ab40db6ae3850454
Stephen Wright <wright.net@gmail.com>
2011-02-09 00:32:44 -0600

    updated how public views recent blog posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   
   def index
-     if (user_signed_in?)
-        @posts = Post.published_for_members.order('published_at DESC').limit(3)
-      else
-        @posts = Post.published :order => 'published_at DESC', :limit => 3
-      end
+    if (user_signed_in?)
+      @posts = Post.published_for_members.order('published_at DESC').limit(3)
+    else
+      @posts = Post.published.order('published_at DESC').limit(3)
+    end
   end    
   
   def about    
