77721c130edcc927ceeabf6bc0620886efdb9cf6
Stephen Wright <wright.net@gmail.com>
2011-03-08 19:31:02 -0600

    changed how s3 determines how to get the key for S3

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,4 @@
-if Rails.env == "production"
+if !ENV['S3_KEY'].nil?
   # set credentials from ENV hash
   S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] }
 else
