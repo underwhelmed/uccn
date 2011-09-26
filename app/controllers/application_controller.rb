917643c639073efae8935721af713174e424d52a
Stephen Wright <wright.net@gmail.com>
2011-09-25 23:26:25 -0500

    added render_404 method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class ApplicationController < ActionController::Base
   def validate_admin 
     redirect_to root_path, :notice => 'You are not authorized to view this page' if current_user.nil? or !current_user.account_active or !current_user.admin 
   end
+  
+  def render_404
+    respond_to do |format|
+       format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
+       format.xml  { head :not_found }
+       format.any  { head :not_found }
+     end
+  end
    
 end
