b42b6e6cdfe3f485f3c068157134f50668462c0d
Stephen Wright <wright.net@gmail.com>
2012-02-11 13:58:41 -0600

    updated view of gallery to display images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,5 @@ Uccnrails::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  
-  Paperclip.options[:command_path] = '/usr/local/bin'
 
 end
