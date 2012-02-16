200c4374d2ee9e58411c91e510394577777edc19
Stephen Wright <wright.net@gmail.com>
2012-02-16 14:43:21 -0600

    removed header image to test out rendering pdf since it's not even in the pdf anyway (display:none)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,12 +12,5 @@ module ApplicationHelper
     # when generating a PDF, PDFKit::Middleware will set this flag
     request.env["Rack-Middleware-PDFKit"] == "true"
   end
-  
-  def pdf_image_tag(filename, options = {})
-    path = Rails.root.join("../public/images/#{filename}")
-    options[:src] = path.to_s
-    attributes = options.map{ |k,v| "#{k}='#{v}'" }.join(" ")
-    raw("<img #{attributes}/>")
-  end
- 
+     
 end
