fe8369dd64f93ba6b340aae36e466159cdf10b82
Stephen Wright <wright.net@gmail.com>
2012-02-16 16:33:58 -0600

    changed the name of the session so cookies would not collide with previous version

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Uccnrails::Application.config.session_store :cookie_store, :key => '_uccnrails_session'
+Uccnrails::Application.config.session_store :cookie_store, :key => '_uccn_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
